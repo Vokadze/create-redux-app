8bd6c0d34e8d0811c36a5cfa4d3e972d4cbb7e93
Ezdakov Sergey <esdakov@mail.ru>
2023-05-20 15:18:15 +0300

    lesson 18 Redux part 1. exercise 1

--- src/index.js
+++ src/index.js
@@ -23,6 +23,10 @@ const App = (params) => {
     store.dispatch(actions.titleCganged(taskId))
   }
 
+  const deleteTask = (taskId) => {
+    store.dispatch(actions.taskDeleted(taskId))
+  }
+
   return (
     <>
       <h1>App</h1>
@@ -33,6 +37,7 @@ const App = (params) => {
             <p>{`Completed: ${el.completed}`}</p>
             <button onClick={() => completedTask(el.id)}>Completed</button>
             <button onClick={() => changeTitle(el.id)}>Change title</button>
+            <button onClick={() => deleteTask(el.id)}>Delete task</button>
             <hr />
           </li>
         ))}
